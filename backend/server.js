1befc15b75513676caa878b68396ee5b9179b471
janmichaelvalle <janmichaelvalle@gmail.com>
2024-04-03 10:53:42 +0800

    Add server.js and db.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './config/db.js';
 import products from './data/products.js'
 const port = process.env.PORT || 5001;
 
+connectDB() // Connect to MongoDB
+
 const app = express ();
+
 
 app.get('/', (req, res) => {
   res.send("API is running...")
@@ -13,7 +17,7 @@ app.get('/', (req, res) => {
 app.get('/api/products', (req, res) => {
   res.json(products);
 });
-¡
+
 app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product)
