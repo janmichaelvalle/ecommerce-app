ee35160b4020dfaebf287a59b915e7cb013a15e3
janmichaelvalle <janmichaelvalle@gmail.com>
2024-04-04 21:08:04 +0800

    Add models and seeder file

---
+++ backend/seeder.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from 'colors';
+import users from "./data/users";
+import products from './data/products';
+import User from './models/productModel.js'
+import Product from "./models/productModel.js";
+import Order from './models/orderModel.js'
+import connectDB from './config/db.js'
+
+dotenv.config();
+
+connectDB();
+
+
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+  } catch (error) {
+    
+  }
+}
